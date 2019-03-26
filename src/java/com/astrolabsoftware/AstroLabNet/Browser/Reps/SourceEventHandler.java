934b3193156d2cb7034072a26edec5bd22ceb26b
Julius Hrivnac <julius.hrivnac@cern.ch>
2019-03-26 17:15:03 +0100

    search preparing

---
+++ src/java/com/astrolabsoftware/AstroLabNet/Browser/Reps/SourceEventHandler.java
@@ -0,0 +1,37 @@
+package com.astrolabsoftware.AstroLabNet.Browser.Reps;
+
+import com.astrolabsoftware.AstroLabNet.Browser.Components.*;
+
+// JavaFX
+import javafx.event.EventHandler;
+import javafx.event.ActionEvent;
+
+// org.json
+
+// Java
+
+// Log4J
+
+/** <code>SourceEventHandler</code> implements {@link EventHandler} for {@link SourceRep}.
+  * It executes {@link Action}.
+  * @opt attributes
+  * @opt operations
+  * @opt types
+  * @opt visibility
+  * @author <a href="mailto:Julius.Hrivnac@cern.ch">J.Hrivnac</a> */
+public class SourceEventHandler implements EventHandler<ActionEvent> {
+  
+  /** Create.
+    * @param sourceRep The associated {@link SourceRep}. */
+  public SourceEventHandler(SourceRep sourceRep) {
+    _sourceRep = sourceRep;
+    }
+    
+  @Override
+  public void handle(ActionEvent event) {
+    _sourceRep.addTab();
+    }
+    
+  private SourceRep _sourceRep;  
+
+  }
--- src/java/com/astrolabsoftware/AstroLabNet/Browser/Reps/SourceRep.java
+++ src/java/com/astrolabsoftware/AstroLabNet/Browser/Reps/SourceRep.java
@@ -4,6 +4,26 @@ import com.astrolabsoftware.AstroLabNet.Browser.BrowserWindow;
 import com.astrolabsoftware.AstroLabNet.Browser.Components.*;
 import com.astrolabsoftware.AstroLabNet.DB.*;
 
+// JavaFX
+import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
+import javafx.scene.control.SplitPane;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.Tab;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.control.ScrollPane;
+import javafx.event.EventHandler;
+import javafx.event.ActionEvent;
+import javafx.geometry.Pos;
+import javafx.geometry.Orientation;
+
 // Java
 import java.util.List;
 import java.util.Map;
@@ -44,6 +64,95 @@ public class SourceRep extends ElementRep {
     super(source, browser, Images.SOURCE);
     }
     
+  /** Give the keeping {@link ServerRep}.
+    * @return The keeping {@link ServerRep}. */
+  public ServerRep serverRep() {
+    return ServerRep.create(server(), browser());
+    }
+    
+  @Override
+  public List<MenuItem> menuItems() {
+    List<MenuItem> menuItems = super.menuItems();
+    MenuItem execute = new MenuItem("Prepare for Search",  Images.icon(Images.SESSION));
+    execute.setOnAction(new SourceEventHandler(this));
+    menuItems.add(execute);
+    return menuItems;
+    }
+    
+  /** Set reference to result.
+    * To be filled once available.
+    * @param resultRef The result {@link TextFlow} shown in the {@link SourceRep} tab.*/
+  public void setResultRef(TextFlow resultRef) {
+    _resultRef = resultRef;
+    }
+    
+  /** Set result into result reference on the {@link SessionRep} tab.
+    * @param result The result {@link Text}. */
+  public void setResult(Text result) {
+    if (_resultRef == null) {
+      addTab();
+      }
+    else {
+      _resultRef.getChildren().add(result);
+      }
+    }
+    
+  /** Add {@link Tab} of this SourceRep. */
+  public void addTab() {/*
+    // Desc
+    Label desc = new Label("Command in " + language() + ":");
+    // Cmd
+    TextArea cmd = new TextArea();
+    cmd.setPrefHeight(2000);
+    // Progress
+    _progress = new ProgressBar(0);
+    // Button
+    Button button = new Button("Execute");
+    // ButtonBox = Progress + Button
+    HBox buttonBox = new HBox(10);
+    buttonBox.setSpacing(5);
+    buttonBox.setAlignment(Pos.CENTER);
+    buttonBox.getChildren().addAll(_progress, button);
+    // CmdBox = Desc + Cmd + ButtonBox 
+    VBox cmdBox = new VBox();
+    cmdBox.setSpacing(5);
+    cmdBox.setAlignment(Pos.CENTER);
+    cmdBox.getChildren().addAll(desc, cmd, buttonBox);
+    // ResultText
+    TextFlow resultText = new TextFlow(); 
+    Text result0 = new Text("Fill in or select Action\n\n");
+    result0.setFill(Color.DARKGREEN);
+    resultText.getChildren().add(result0);
+    // ScrollPane = ResultText
+    ScrollPane scrollPane = new ScrollPane();
+    scrollPane.setFitToWidth(true);
+    scrollPane.setContent(resultText);
+    // Pane = Desc + Cmd + ButtonBox + ScrollPane
+    SplitPane pane = new SplitPane();
+    pane.setDividerPositions(0.5);
+    pane.setOrientation(Orientation.VERTICAL);
+    pane.getItems().addAll(cmdBox, scrollPane);
+    SessionRep sessionRep = this;
+    button.setOnAction(new EventHandler<ActionEvent>() {
+      @Override
+      public void handle(ActionEvent e) {
+        String result = serverRep().livy().sendCommand(id(), cmd.getText());
+        int id = new JSONObject(result).getInt("id");
+        browser().command().addTask(serverRep().name() + "/" + id() + "/" + id, sessionRep.session(), id);
+        resultText.getChildren().add(new Text("Command send to Session\n\n"));
+        }
+      });
+    setResultRef(resultText);
+    Tab tab = browser().addTab(pane, toString(), Images.SESSION);
+    browser().registerSessionTab(this, tab);*/
+    }
+
+  /** Give the SessionRep keeping {@link Server}.
+    * @return The SessionRep keeping {@link Server}. */
+  public Server server() {
+    return source().server();
+    }
+    
   /** Give the referenced {@link Source}.
     * @return The referenced {@link Source}. */
   public Source source() {
@@ -54,6 +163,8 @@ public class SourceRep extends ElementRep {
   public String toString() {
     return source().toString();
     }
+  
+  private TextFlow _resultRef;
    
   /** Logging . */
   private static Logger log = Logger.getLogger(SourceRep.class);
