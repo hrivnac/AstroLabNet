b507235ac227abd1fc3205c05c3e809975e7df91
Julius Hrivnac <julius.hrivnac@cern.ch>
2019-07-01 15:51:10 +0200

    HBase Search in WS

--- ant/setup.sh
+++ ant/setup.sh
@@ -2,3 +2,5 @@ if [[ "x" = "x${ANT_HOME}" ]]; then
   export ANT_HOME="${ant}"
   export PATH="${ANT_HOME}/bin:${PATH}"
   fi
+source build-global.properties
+
---
+++ src/sh/fillHBase.sh
@@ -0,0 +1,6 @@
+/opt/hbase/bin/hbase shell ../src/ruby/createCatalog.rb
+/opt/hbase/bin/hbase shell ../src/ruby/createJournal.rb
+/opt/hbase/bin/hbase shell ../src/ruby/createTopology.rb
+/opt/hbase/bin/hbase shell ../src/ruby/fillTopology.rb
+java -jar ../lib/AvroReader-${version}.exe.jar http://localhost:8080 ../../fink-broker/datasim
+
